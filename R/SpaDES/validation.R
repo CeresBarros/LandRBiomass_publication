30ea2e14bfd72cc21252c90aec602abd161978da
CeresBarros <cbarros@mail.ubc.ca>
2019-12-18 16:36:11 -0800

    validation plots

--- R/SpaDES/validation.R
+++ R/SpaDES/validation.R
@@ -236,248 +236,146 @@ names(speciesColours) <- levels(vegTypeMapStk[[1]])[[1]]$VALUE
 
 ## calculate everything at landscape level
 
+## LANDSCAPE-WIDE COMPARISONS IN A GIVEN YEAR --------------------
+## relative abundance (biomass) per species
+plotData <- standCohortData[year == 10,
+                            list(landRelativeAbund = sum(B, na.rm = TRUE)/unique(landscapeB),
+                                 landRelativeAbundValid = sum(BValid, na.rm = TRUE)/unique(landscapeBValid)),
+                            by = .(rep, speciesCode)]
 plot1 <- ggplot(data = plotData,
-                aes(x = speciesCode, y = relativeAbund)) +
+                aes(x = speciesCode, y = landRelativeAbund)) +
   stat_summary(fun.y = "mean", geom = "bar") +
   stat_summary(fun.data = "mean_sd", geom = "linerange", size = 1) +
-  stat_summary(aes(y = relativeAbundValid),
+  stat_summary(aes(y = landRelativeAbundValid),
                fun.y = "mean", geom = "point") +
   scale_x_discrete(labels = speciesLabels) +
   theme_pubr() +
-  labs(title = "Species mean relative abundance", y = "species B / stand B")
+  labs(title = "Landscape-wide species relative abundances",
+       x = "", y = expression(over("species B", "total B")))
 
-plot2 <- ggplot(data = summaryAllCohortData,
-                aes(x = Year, y = log(MortalityBySpecies), colour = speciesCode)) +
-  geom_vline(xintercept = 5, size = 1.5, linetype = "dashed", colour = "red") +
-  geom_line(size = 1.5) +
-  theme_classic() +
-  theme(text = element_text(size = 16),
-        legend.title = element_blank()) +
-  scale_colour_manual(values = speciesColours,
-                      labels = speciesLabels) +
-  facet_grid(burnt ~ Scenario,
-             labeller = labeller(burnt = c("0" = "no fire", "1" = "fire")))
+## no. pixels with a species
+plotData <- standCohortData[year == 10, list(count = sum(B > 0),
+                                             countValid = sum(BValid > 0)),
+                            by = .(rep, speciesCode)]
+plotData <- melt(plotData, measure.vars = c("count", "countValid"),
+                 variable.name = "dataType", value.name = "count")
 
-plot3 <- ggplot(data = pixelBurnCohortData,
-                aes(x = Year, fill = vegType)) +
-  geom_area(stat = "count", position = "fill") +
-  geom_vline(xintercept = 5, size = 1.5, linetype = "dashed", colour = "red") +
-  scale_fill_manual(values = speciesColours,
-                    labels = speciesLabels) +
-  theme_classic() +
-  theme(text = element_text(size = 16),
-        legend.title = element_blank()) +
-  labs(title = "No. pixels per vegetation type", y = "g/m^2") +
-  facet_grid(burnt ~ Scenario,
-             labeller = labeller(burnt = c("0" = "no fire", "1" = "fire")))
+## HERE: TODO: THE NUMBER OF PIXELS IN VALID IS VARYING BETWEENS REPS FOR THE SAME SPECIES.
+plot2 <- ggplot(data = plotData[dataType == "count"],
+                aes(x = speciesCode, y = count)) +
+  stat_summary(fun.y = "mean", geom = "bar") +
+  stat_summary(fun.data = "mean_sd", geom = "linerange", size = 1) +
+  stat_summary(data = plotData[dataType == "countValid"],
+               aes(x = speciesCode, y = count),
+               fun.y = "mean", geom = "point") +
+  scale_x_discrete(labels = speciesLabels) +
+  theme_pubr() +
+  labs(title = "Species presences",
+       x = "", y = "no. pixels",
+       fill = "")
 
-plot4 <- ggplot(data = summaryAllCohortData,
-                aes(x = Year, y = AgeBySppWeighted, colour = speciesCode)) +
-  geom_vline(xintercept = 5, size = 1.5, linetype = "dashed", colour = "red") +
-  geom_line(size = 1.5) +
-  theme_classic() +
-  theme(text = element_text(size = 16),
-        legend.title = element_blank()) +
-  scale_colour_manual(values = speciesColours,
-                      labels = speciesLabels) +
-  facet_grid(burnt ~ Scenario,
-             labeller = labeller(burnt = c("0" = "no fire", "1" = "fire")))
+plot3 <- ggplot(data = plotData, aes(x = speciesCode, y = count, fill = dataType)) +
+ geom_boxplot() +
+  scale_x_discrete(labels = speciesLabels) +
+  scale_fill_discrete(labels = c("count" = "simulated",
+                                 "countValid" = "observed")) +
+  theme_pubr() +
+  labs(title = "Species presences",
+       x = "", y = "no. pixels",
+       fill = "")
 
-plot5 <- ggplot(data = summaryAllCohortData,
-                aes(x = Year, y = noCohorts, colour = speciesCode)) +
-  geom_vline(xintercept = 5, size = 1.5, linetype = "dashed", colour = "red") +
-  geom_line(size = 1.5) +
-  theme_classic() +
-  theme(text = element_text(size = 16),
-        legend.title = element_blank()) +
-  scale_colour_manual(values = speciesColours,
-                      labels = speciesLabels) +
-  facet_grid(burnt ~ Scenario,
-             labeller = labeller(burnt = c("0" = "no fire", "1" = "fire")))
+## relative abundance per dominant species
+## get dominant species - these match with model outputs, I checked
+plotData <- unique(standCohortData[year == 10,
+                            list(vegTypeID2 = speciesCode[which.max(B)],
+                                 B = max(B, na.rm = TRUE),
+                                 landscapeB = unique(landscapeB),
+                                 vegTypeID2Valid = speciesCode[which.max(BValid)],
+                                 BValid = max(BValid, na.rm = TRUE),
+                                 landscapeBValid = unique(landscapeBValid)),
+                            by = .(rep, pixelIndex)])
 
-ggpubr::ggarrange(plot1, plot2, plot3, nrow = 3,
-                  legend = "right", common.legend = TRUE)
-ggsave(filename = "R/SpaDES/outputs/blogSep2019_noPM_PM_BMortVegType.tiff",
-       width = 10, height = 15)
+## calculate relative abundances and melt data.
+plotData[, landRelativeAbund := sum(B, na.rm = TRUE)/unique(landscapeB),
+         by = .(rep, vegTypeID2)]
+plotData[, landRelativeAbundValid := sum(BValid, na.rm = TRUE)/unique(landscapeBValid),
+         by = .(rep, vegTypeID2Valid)]
+plotData1 <- plotData[, .(rep, pixelIndex, vegTypeID2, B, landscapeB, landRelativeAbund)]
+plotData1[, dataType := "simulated"]
+plotData2 <- plotData[, .(rep, pixelIndex, vegTypeID2Valid, BValid,
+                          landscapeBValid, landRelativeAbundValid)]
+plotData2[, dataType := "observed"]
+setnames(plotData2, grep("Valid", names(plotData2), value = TRUE),
+         sub("Valid", "", grep("Valid", names(plotData2), value = TRUE)))
 
-## GIFS ------------------------------------------------------
-## make GIFs of vegetation maps
-## individual pics first
-speciesLabels <- c("Fir", "Larch", "En. spruce", "Wh. spruce",
-                   "Bl. spruce", "Lo. pine", "Aspen","Douglas-fir")
-names(speciesLabels) <- levels(vegTypeMapStk_noPM[[1]])[[1]]$ID
-speciesColours <- levels(vegTypeMapStk_noPM[[1]])[[1]]$colors
-names(speciesColours) <-  levels(vegTypeMapStk_noPM[[1]])[[1]]$ID
+plotData <- rbind(plotData1, plotData2)
+rm(plotData1, plotData2)
 
-foothillsMask <- simList_noPM$rawBiomassMap
-foothillsMask[!is.na(foothillsMask)] <- 1
-foothillsMaskDF <- as.data.frame(as(foothillsMask, "SpatialPixelsDataFrame"))
-names(foothillsMaskDF) <- c("value", "x", "y")
+plot2 <- ggplot(data = plotData[dataType == "simulated"],
+                aes(x = vegTypeID2, y = landRelativeAbund)) +
+  stat_summary(fun.y = "mean", geom = "bar") +
+  stat_summary(fun.data = "mean_sd", geom = "linerange", size = 1) +
+  stat_summary(data = plotData[dataType != "simulated"],
+               aes(x = vegTypeID2, y = landRelativeAbund),
+               fun.y = "mean", geom = "point") +
+  scale_x_discrete(labels = speciesLabels) +
+  theme_pubr() +
+  labs(title = "Landscape-wide relative abundances of dominant species",
+       x = "", y = expression(over("species B", "total B")))
 
-makePNGs <- function(id, rasterStack, filePrefix, gif.dir) {
-  suppressWarnings(dir.create(gif.dir, recursive = TRUE))
-  cat("Making ", id, "\n")
-  rasterVis::gplot(rasterStack[[id]],
-                   maxpixels = ncell(rasterStack[[id]])) +
-    geom_tile(data = foothillsMaskDF,
-              aes(x = x, y = y), fill = "grey95") +
-    geom_tile(aes(fill = as.factor(value))) +
-    scale_fill_manual(values = speciesColours,
-                      labels = speciesLabels) +
-    theme_void() + theme(legend.position = "none", text = element_text(size = 20)) +
-    labs(title = sub("year", "Year ", names(rasterStack)[id])) +
-    coord_equal()
-  ggsave(file.path(gif.dir, paste0(filePrefix, id, ".png")),
-         device = "png", width=5, height=10, dpi = 300, units = "in")
-}
+## no. pixels with a certain dominant species
+plot3 <- ggplot(data = plotData, aes(x = vegTypeID2, fill = dataType)) +
+  geom_histogram(stat = "count", position = "dodge") +
+  scale_x_discrete(labels = speciesLabels) +
+  theme_pubr() +
+  labs(title = "Landscape-wide species dominance",
+       x = "", y = "no. pixels",
+       fill = "")
 
-map_df(.x = 1:nlayers(vegTypeMapStk_noPM), .f = makePNGs,
-       rasterStack = vegTypeMapStk_noPM,
-       filePrefix = "vegTypeMapStk_noPM",
-       gif.dir = "R/SpaDES/outputs/blogSep2019_noPM_oneFire/gif")
-map_df(.x = 1:nlayers(vegTypeMapStk_PM), .f = makePNGs,
-       rasterStack = vegTypeMapStk_PM,
-       filePrefix = "vegTypeMapStk_PM",
-       gif.dir = "R/SpaDES/outputs/blogSep2019_PM_oneFire/gif")
+## as previous, but in relative terms
+plot4 <- ggplot(data = plotData, aes(x = dataType, fill = vegTypeID2)) +
+  geom_histogram(stat = "count", position = "fill") +
+  scale_fill_brewer(palette = "Dark2", labels = speciesLabels) +
+  theme_pubr() +
+  labs(title = "Landscape-wide species dominance", x = "",
+       y = "proportion of pixels", fill = "dominant species")
 
-makeGIF <- function(gif.dir, gifPrefix, ...) {
-  ## get file list and the file numbers (to sort numerically, rather than alphabetically)
-  PNGlist <- list.files(path = gif.dir, pattern = "*.png")
-  fileNos <- as.numeric(sub("\\.png", "", sub("^\\D*(\\d)", "\\1", PNGlist)))
+grid.arrange(plot1, plot2, plot3, plot4,
+             nrow = 2, ncol = 2)
 
-  ## make GIF
-  file.path(gif.dir, PNGlist[order(fileNos)]) %>%
-    map(image_read) %>% # reads each path file
-    image_join() %>% # joins image
-    image_animate(...) %>% # animates, can opt for number of loops
-    image_write(file.path(gif.dir, paste0(gifPrefix, ".gif")))
-}
+## COMPARISONS OF DELTA PER PIXEL-------------------
+## per species
+plotData <- standCohortData[year %in% c(0,10),]
+plotData <- plotData[, list(deltaB = B[which(year == 10)] - B[which(year == 0)],
+                            deltaBValid = BValid[which(year == 10)] - BValid[which(year == 0)]),
+                     , by = .(rep, pixelIndex, speciesCode)]
+plotData <- melt(plotData, measure.vars = c("deltaB", "deltaBValid"),
+                 variable.name = "dataType", value.name = "deltaB")
 
-makeGIF(gif.dir = "R/SpaDES/outputs/blogSep2019_noPM_oneFire/gif",
-        gifPrefix = "vegTypeMapStk_noPM",
-        fps = 2)
-makeGIF(gif.dir = "R/SpaDES/outputs/blogSep2019_PM_oneFire/gif",
-        gifPrefix = "vegTypeMapStk_PM",
-        fps = 2)
-
-## OTHER PLOTS --------------------------------------------------------
-## topo and climate examples
-slopeRas <- projectRaster(simList_noPM$slopeRas, foothillsMask)
-topoPal <- colorRampPalette(RColorBrewer::brewer.pal(n = 9, name = "BrBG"))
-plot(foothillsMask, col = "grey90", axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE)
-plot(slopeRas, col = topoPal(20), axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE, add = TRUE)
-
-temperatureRas <- projectRaster(simList_noPM$temperatureRas, foothillsMask)
-tempPal <- colorRampPalette(RColorBrewer::brewer.pal(n = 9, name = "RdBu"))
-plot(foothillsMask, col = "grey90", axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE)
-plot(temperatureRas, col = tempPal(20), axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE, add = TRUE)
-
-precipitationRas <- projectRaster(simList_noPM$precipitationRas, foothillsMask)
-plot(foothillsMask, col = "grey90", axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE)
-plot(precipitationRas, col = tempPal(20), axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE, add = TRUE)
-
-## spp cover, age and biomass examples
-sppPal <- colorRampPalette(RColorBrewer::brewer.pal(n = 9, name = "Blues"))
-plot(foothillsMask, col = "grey90", axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE)
-plot(simList_noPM$speciesLayers[[1]], col = sppPal(20), axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE, add = TRUE)
-
-plot(foothillsMask, col = "grey90", axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE)
-plot(simList_noPM$rawBiomassMap, axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE, add = TRUE)
-
-plot(foothillsMask, col = "grey90", axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE)
-agePal <- colorRampPalette(RColorBrewer::brewer.pal(n = 9, name = "Greens"))
-plot(simList_noPM$standAgeMap, col = agePal(20), axes = FALSE,
-     bg = "transparent", box = FALSE, legend = FALSE, add = TRUE)
-
-## ecodistricts
-## https://www.statcan.gc.ca/eng/subjects/standard/environment/elc/12-607-x2018001-eng.pdf
-
-ecoDistSF <- sf::st_as_sf(simList_noPM$ecoDistrict)
-ecoDistSF$ECODISTRIC <- factor(ecoDistSF$ECODISTRIC,
-                               levels = c(798, 800, 801, 799, 793, 750, 631, 1018, 1017, 1019))
-canada <- sf::st_as_sf(shapefile("data/CA_admin/gpr_000a11a_e.shp"))
-canada <- sf::st_transform(canada, crs = crs(ecoDistSF))
-alberta <- canada[canada$PRENAME %in% "Alberta",]
-
-ggplot(ecoDistSF) +
-  geom_sf(data = alberta) +
-  geom_sf(data = ecoDistSF, aes(fill = as.factor(ECODISTRIC))) +
-  scale_fill_brewer(palette = "Paired",
-                    labels = c("631" = "W AB upland - Foothills",
-                               "750" = "Aspen parkland - Upland",
-                               "793" = "Moist mixed grassland - Plain",
-                               "798" = "Fescue grassland - Plain",
-                               "799" = "Fescue grassland - Upland",
-                               "800" = "Fescue grassland - Plain",
-                               "801" = "Fescue grassland - Foothills",
-                               "1017" = "N Cont. Divide - Mountains",
-                               "1018" = "N Cont. Divide - Foothills",
-                               "1019" = "N Cont. Divide - Mountains")) +
-  theme_void() +
-  theme(text = element_text(colour = "white"),
-        plot.background = element_rect(fill = "black")) +
-  labs(fill = "Ecoregion - ecodistrict") +
-  coord_sf()
-
-plot(sf::st_as_sf(simList_noPM$ecoDistrict["ECODISTRIC"]))
+plot5 <- ggplot(data = plotData,
+                aes(x = speciesCode, y = deltaB, fill = dataType)) +
+  geom_boxplot() +
+  scale_x_discrete(labels = speciesLabels) +
+  scale_fill_discrete(labels = c("deltaB" = "simulated",
+                                 "deltaBValid" = "observed")) +
+  theme_pubr() +
+  labs(title = "Pixel changes in abundances", fill = "",
+       x = "", y = expression(paste(Delta, "B")))
 
 
-save(list = grep("model", ls(), value = TRUE), file = "E:/GitHub/LandscapesInMotion/analyses/modelsGAMLSS_0-3Days_goodSample_Nov1_v2.RData")
+plotData <- plotData[, mean(deltaB) , by = .(rep, speciesCode, dataType)]
 
+plot6 <- ggplot(data = plotData[dataType == "deltaB"],
+                aes(x = speciesCode, y = V1)) +
+  stat_summary(fun.y = "mean", geom = "bar") +
+  stat_summary(fun.data = "mean_sd", geom = "linerange", size = 1) +
+  stat_summary(data = plotData[dataType == "deltaBValid"],
+               aes(x = speciesCode, y = V1),
+               fun.y = "mean", geom = "point") +
+  scale_x_discrete(labels = speciesLabels) +
+  theme_pubr() +
+  labs(title = "Landscape-wide changes in abundances",
+       x = "", y = expression(paste(Delta, "B")))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-## list outputs of one repetition
-outputs(factorialSimulations$sim1_rep01)
-# Try loading one object from output(sim)
-# b <- readRDS(outputs(factorialSimulations$sim1_rep01)$file[1])
-
-# Now run the code snippet to make the figure. This was taken directly from ?experiment2
-fn <- quote({
-  pixelCohortData <- addNoPixel2CohortData(cohortData, pixelGroupMap)
-  pixelCohortData[, list(BiomassBySpecies = as.numeric(sum(B * noPixels, na.rm = TRUE)),
-                         MortalityBySpecies = as.numeric(sum(mortality * noPixels, na.rm = TRUE)),
-                         aNPPBySpecies = as.numeric(sum(aNPPAct * noPixels, na.rm = TRUE)),
-                         AgeBySppWeighted = as.numeric(sum(age * B * noPixels, na.rm = TRUE) /
-                                                         sum(B * noPixels, na.rm = TRUE)),
-                         noCohorts = as.numeric(length(unique(age)))),
-                  by = speciesCode] %>%
-    as.list(.)
-})
-b <- as.data.table(factorialSimulations,
-                   vals = fn,
-                   objectsFromOutputs = lapply(1:5, function(x) c("cohortData", "pixelGroupMap")))
-p <- ggplot(b, aes(x=simList, y=NBurnedPixels, group=simList, color=simList)) +
-  stat_summary(geom = "point", fun.y = mean) +
-  stat_summary(geom = "errorbar", fun.data = mean_se, width = 0.2)
-print(p)
+grid.arrange(plot5, plot6,
+             ncol = 2)
