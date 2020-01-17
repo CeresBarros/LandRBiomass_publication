c404543c8b2adbaa4b71e84dc8119ddf16a926db
CeresBarros <cbarros@mail.ubc.ca>
2020-01-16 16:59:04 -0800

    new script to get objects for validation

---
+++ R/SpaDES/3_simObjects4Valid.R
@@ -0,0 +1,26 @@
+RTM <- postProcess(simOutSpeciesLayers$rasterToMatchLarge,
+                   studyArea = if (grepl("study", runName)) get(runName) else studyAreaS,
+                   rasterToMatch = simOutSpeciesLayers$rasterToMatchLarge,
+                   useSAcrs = FALSE,
+                   maskWithRTM = FALSE,
+                   useCache = FALSE,
+                   method = "bilinear",
+                   filename2 = NULL)
+RTM[!is.na(RTM)] <- 1
+
+rstLCChangeAllbin <- unzip(file.path(options()$reproducible.inputPaths, "LCchange_AlldisturbancesBinary.zip"),
+                           exdir = file.path(options()$reproducible.inputPaths))
+rstLCChangeAllbin <- raster(file.path(options()$reproducible.inputPaths, "change_2001_2011.dat"))
+
+rstLCChangeAllbin <- postProcess(x = rstLCChangeAllbin,
+                                 studyArea = if (grepl("study", runName)) get(runName) else studyAreaS,
+                                 rasterToMatch = RTM,
+                                 useSAcrs = FALSE,
+                                 maskWithRTM = TRUE,
+                                 method = "ngb",
+                                 filename2 = file.path(options()$reproducible.inputPaths, "change_2001_2011_postProcess.tiff"),
+                                 overwrite = TRUE,
+                                 cacheRepo = simPaths$cachePath,
+                                 useCache = TRUE)
+rm(RTM); amc::.gc()
+
