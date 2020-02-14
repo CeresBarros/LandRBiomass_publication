5ecae85e9310aa0f4f215a3bf0f48d18db56fbb9
CeresBarros <cbarros@mail.ubc.ca>
2020-02-14 15:48:28 -0800

    get user-supplied rstLCChange from GDdrive

--- R/SpaDES/3_simObjects4Valid.R
+++ R/SpaDES/3_simObjects4Valid.R
@@ -8,20 +8,22 @@ RTM <- postProcess(simOutSpeciesLayers$rasterToMatchLarge,
                    filename2 = NULL)
 RTM[!is.na(RTM)] <- 1
 
-rstLCChangeAllbin <- unzip(file.path(options()$reproducible.inputPaths, "LCchange_AlldisturbancesBinary.zip"),
-                           exdir = file.path(options()$reproducible.inputPaths))
-rstLCChangeAllbin <- raster(file.path(options()$reproducible.inputPaths, "change_2001_2011.dat"))
-
-rstLCChangeAllbin <- postProcess(x = rstLCChangeAllbin,
-                                 studyArea = if (grepl("study", runName)) get(runName) else studyAreaS,
-                                 rasterToMatch = RTM,
-                                 useSAcrs = FALSE,
-                                 maskWithRTM = TRUE,
-                                 method = "ngb",
-                                 filename2 = file.path(options()$reproducible.inputPaths, "change_2001_2011_postProcess.tiff"),
-                                 overwrite = TRUE,
-                                 cacheRepo = simPaths$cachePath,
-                                 useCache = TRUE)
+rstLCChangeAllbin <- Cache(prepInputs,
+                           targetFile = "change_2001_2011.dat",
+                           archive = "LCchange_AlldisturbancesBinary.zip",
+                           url = "https://drive.google.com/file/d/1xd46zkJRdZVVNK93AUcfqUvtL-OFb4h4/view?usp=sharing",
+                           alsoExtract = "similar",
+                           fun = "raster::raster",
+                           destinationPath = options()$reproducible.inputPaths,
+                           studyArea = if (grepl("study", runName)) get(runName) else studyAreaS,
+                           rasterToMatch = RTM,
+                           useSAcrs = FALSE,
+                           maskWithRTM = TRUE,
+                           method = "ngb",
+                           filename2 = file.path(options()$reproducible.inputPaths, "change_2001_2011_postProcess.tiff"),
+                           overwrite = TRUE,
+                           cacheRepo = simPaths$cachePath,
+                           useCache = TRUE)
 
 ## convert to mask
 rstLCChangeAllbin[getValues(rstLCChangeAllbin) != 1] <- NA
