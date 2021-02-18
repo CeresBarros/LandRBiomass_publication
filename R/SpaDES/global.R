a6c1196f1e798a883830a7c0e8feb31de227fdf2
CeresBarros <cbarros@mail.ubc.ca>
2021-02-17 17:35:37 -0800

    don't use logB in modelBiomass

--- R/SpaDES/global.R
+++ R/SpaDES/global.R
@@ -80,8 +80,6 @@ simParams <- list(
     , "forestedLCCClasses" = c(1:15, 34:35)
     , "LCCClassesToReplaceNN" = c(34:35)
     , "fitDeciduousCoverDiscount" = TRUE
-    # , "biomassModel" =  quote(lme4::lmer(logB ~ logAge * speciesCode + cover * speciesCode +
-    #                                        (logAge + cover | ecoregionGroup)))
     , "biomassModel" =  quote(lme4::lmer(B ~ logAge * speciesCode + cover * speciesCode +
                                            (logAge + cover | ecoregionGroup)))
     , "exportModels" = "all"
