1ed17ebd8287d19c06ec5faa72889be01ece52a7
CeresBarros <cbarros@mail.ubc.ca>
2021-02-18 10:40:15 -0800

    don't subset data

--- R/SpaDES/global.R
+++ R/SpaDES/global.R
@@ -76,6 +76,8 @@ simParams <- list(
     , "forestedLCCClasses" = c(1:15, 34:35)
     , "LCCClassesToReplaceNN" = c(34:35)
     , "fitDeciduousCoverDiscount" = TRUE
+    , "subsetDataAgeModel" = FALSE
+    , "subsetDataBiomassModel" = FALSE
     , "biomassModel" =  quote(lme4::lmer(B ~ logAge * speciesCode + cover * speciesCode +
                                            (logAge + cover | ecoregionGroup)))
     , "exportModels" = "all"
