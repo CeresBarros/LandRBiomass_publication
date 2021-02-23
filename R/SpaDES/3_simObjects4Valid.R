152ceb8f93accfad2a5d7febfc4fb109acaad568
CeresBarros <cbarros@mail.ubc.ca>
2021-02-22 17:15:45 -0800

    use events = "init" -> no need for preSims

--- R/SpaDES/3_simObjects4Valid.R
+++ R/SpaDES/3_simObjects4Valid.R
@@ -1,36 +1,36 @@
 ## load simList from simInit()
-if (!exists("LandRBiomass_preSim"))
-  LandRBiomass_preSim <- loadSimList(file.path(simPaths$outputPath, paste0("preSimList_", runName)))
+if (!exists("LandRBiomass_simInit"))
+  LandRBiomass_simInit <- loadSimList(file.path(simPaths$outputPath, paste0("simInit", runName)))
 
 ## load the experiment simLists object
 if (!exists("LandRBiomass_sim"))
   LandRBiomass_sim <- qs::qread(file.path(simPaths$outputPath, paste0("simList_LandRBiomass_sim_", runName)))
 
 ## get necessary rasters from simList (only rasters aren't kept in memory)
-if (!inMemory(LandRBiomass_preSim$rawBiomassMap)) {
-  rasFilename <- raster::filename(LandRBiomass_preSim$rawBiomassMap)
-  rasFilename <- sub(inputPath(LandRBiomass_preSim), simPaths$inputPath, rasFilename)
+if (!inMemory(LandRBiomass_simInit$rawBiomassMap)) {
+  rasFilename <- raster::filename(LandRBiomass_simInit$rawBiomassMap)
+  rasFilename <- sub(inputPath(LandRBiomass_simInit), simPaths$inputPath, rasFilename)
 
   rawBiomassMap <- raster(rasFilename)
 } else {
-  rawBiomassMap <- LandRBiomass_preSim$rawBiomassMap
+  rawBiomassMap <- LandRBiomass_simInit$rawBiomassMap
 }
 
-if (!inMemory(LandRBiomass_preSim$standAgeMap)) {
-  rasFilename <- raster::filename(LandRBiomass_preSim$standAgeMap)
-  rasFilename <- sub(inputPath(LandRBiomass_preSim), simPaths$inputPath, rasFilename)
+if (!inMemory(LandRBiomass_simInit$standAgeMap)) {
+  rasFilename <- raster::filename(LandRBiomass_simInit$standAgeMap)
+  rasFilename <- sub(inputPath(LandRBiomass_simInit), simPaths$inputPath, rasFilename)
 
   standAgeMap <- raster(rasFilename)
 } else {
-  standAgeMap <- LandRBiomass_preSim$standAgeMap
+  standAgeMap <- LandRBiomass_simInit$standAgeMap
 }
 
 ## note that species layers stack can be saved  as a multilayer raster obj (so stack("filename.grd") would work)
 ## HOWEVER because this may NOT be always the case, we're loading them separately anyways
-if (!inMemory(LandRBiomass_preSim$speciesLayers)) {
-  speciesLayers <- lapply(unstack(LandRBiomass_preSim$speciesLayers), function(x) {
+if (!inMemory(LandRBiomass_simInit$speciesLayers)) {
+  speciesLayers <- lapply(unstack(LandRBiomass_simInit$speciesLayers), function(x) {
     rasFilename <- raster::filename(x)
-    rasFilename <- sub(outputPath(LandRBiomass_preSim), simPaths$outputPath, rasFilename)
+    rasFilename <- sub(outputPath(LandRBiomass_simInit), simPaths$outputPath, rasFilename)
     layerName <- names(x)
     sppLayer <- stack(rasFilename)[[layerName]]
 
@@ -39,17 +39,17 @@ if (!inMemory(LandRBiomass_preSim$speciesLayers)) {
   })
   speciesLayers <- stack(speciesLayers)
 } else {
-  speciesLayers <- LandRBiomass_preSim$speciesLayers
+  speciesLayers <- LandRBiomass_simInit$speciesLayers
 }
 
 
-if (!inMemory(LandRBiomass_preSim$rasterToMatch)) {
-  rasFilename <- raster::filename(LandRBiomass_preSim$rasterToMatch)
-  rasFilename <- sub(inputPath(LandRBiomass_preSim), simPaths$inputPath, rasFilename)
+if (!inMemory(LandRBiomass_simInit$rasterToMatch)) {
+  rasFilename <- raster::filename(LandRBiomass_simInit$rasterToMatch)
+  rasFilename <- sub(inputPath(LandRBiomass_simInit), simPaths$inputPath, rasFilename)
 
   standAgeMap <- raster(rasterToMatch)
 } else {
-  rasterToMatch <- LandRBiomass_preSim$rasterToMatch
+  rasterToMatch <- LandRBiomass_simInit$rasterToMatch
 }
 
 rasterToMatch[!is.na(rasterToMatch)] <- 1
@@ -61,7 +61,7 @@ rstLCChangeAllbin <- Cache(prepInputs,
                            alsoExtract = "similar",
                            fun = "raster::raster",
                            destinationPath = options()$reproducible.inputPaths,
-                           studyArea = LandRBiomass_preSim$studyArea,
+                           studyArea = LandRBiomass_simInit$studyArea,
                            rasterToMatch = rasterToMatch,
                            useSAcrs = FALSE,
                            maskWithRTM = TRUE,
@@ -74,7 +74,7 @@ rstLCChangeAllbin <- Cache(prepInputs,
 ## make sure the extent matches the study area (it won't if using a smaller study area than RTMLarge)
 rstLCChangeAllbin <- Cache(postProcess,
                            x = rstLCChangeAllbin,
-                           studyArea = LandRBiomass_preSim$studyArea,
+                           studyArea = LandRBiomass_simInit$studyArea,
                            useSAcrs = FALSE,
                            filename2 = file.path(options()$reproducible.inputPaths, "change_2001_2011_postProcess.tiff"),
                            overwrite = TRUE,
