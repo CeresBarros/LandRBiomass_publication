fe7f8ea8d446d93bd07e70506cadb2a7b3747392
CeresBarros <cbarros@mail.ubc.ca>
2021-08-03 10:56:17 -0700

    code cleanup

--- R/SpaDES/1_simObjects.R
+++ R/SpaDES/1_simObjects.R
@@ -8,53 +8,37 @@
 ## STUDY AREA(S) ---------------------------------------
 
 ## create a larger study area and create a smaller one (half extent)
-## note that projection of the orignal CRS is always necessary
+## note that projection of the original CRS is always necessary
+## smaller area is based on the first
+
+originalcrs <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
+Biomass_corecrs <- "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
 
 if (runName %in% c("baseCase", "altParameters", "studyAreaS", "studyAreaL")) {
   ## set A
-  originalcrs <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
   largeExtent <- extent(-104.757, -104.2197, 55.68663, 56.20319)
-  smallExtent <- largeExtent
-
-  smallExtent@xmax <- largeExtent@xmin + (largeExtent@xmax - largeExtent@xmin)*0.5   ## this is minimum size (10 000 pix at 250m res) -- need to increase large are to double
-  smallExtent@ymax <- largeExtent@ymin + (largeExtent@ymax - largeExtent@ymin)*0.5
-
-  studyAreaL <- as(largeExtent, "SpatialPolygons")
-  studyAreaL <-  SpatialPolygonsDataFrame(studyAreaL, data.frame(id = 1:length(studyAreaL)))
-  crs(studyAreaL) <- originalcrs
-  studyAreaL <- spTransform(studyAreaL, originalcrs)
-
-  studyAreaS <- as(smallExtent, "SpatialPolygons")
-  studyAreaS <-  SpatialPolygonsDataFrame(studyAreaS, data.frame(id = 1:length(studyAreaS)))
-  crs(studyAreaS) <- originalcrs
-  studyAreaS <- spTransform(studyAreaS, originalcrs)
-
 } else if (runName == "studyAreaChange") {
   ## second set of study areas, north of the first (set B)
-  originalcrs <- "+proj=longlat +datum=NAD83 +no_defs +ellps=GRS80 +towgs84=0,0,0"
   largeExtent <- extent(-104.757, -104.2197, 56.25485, 56.77141)
-  smallExtent <- largeExtent
-
-  smallExtent@xmax <- largeExtent@xmin + (largeExtent@xmax - largeExtent@xmin)*0.5   ## this is minimum size (10 000 pix at 250m res) -- need to increase large are to double
-  smallExtent@ymax <- largeExtent@ymin + (largeExtent@ymax - largeExtent@ymin)*0.5
-
-  studyAreaL <- as(largeExtent, "SpatialPolygons")
-  studyAreaL <-  SpatialPolygonsDataFrame(studyAreaL, data.frame(id = 1:length(studyAreaL)))
-  crs(studyAreaL) <- originalcrs
-  studyAreaL <- spTransform(studyAreaL, originalcrs)
-
-  studyAreaS <- as(smallExtent, "SpatialPolygons")
-  studyAreaS <-  SpatialPolygonsDataFrame(studyAreaS, data.frame(id = 1:length(studyAreaS)))
-  crs(studyAreaS) <- originalcrs
-  studyAreaS <- spTransform(studyAreaS, originalcrs)
 } else {
   stop("runName must be one of 'baseCase', 'studyAreaChange', 'altParameters', 'studyAreaS' or 'studyAreaL'")
 }
 
-## now reproject to Biomass_core standard
-Biomass_corecrs <- "+proj=lcc +lat_1=49 +lat_2=77 +lat_0=0 +lon_0=-95 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs +ellps=GRS80 +towgs84=0,0,0"
-studyAreaL <- spTransform(studyAreaL, Biomass_corecrs)
-studyAreaS <- spTransform(studyAreaS, Biomass_corecrs)
+smallExtent <- largeExtent
+smallExtent@xmax <- largeExtent@xmin + (largeExtent@xmax - largeExtent@xmin)*0.5   ## this is minimum size (10 000 pix at 250m res) -- need to increase large are to double
+smallExtent@ymax <- largeExtent@ymin + (largeExtent@ymax - largeExtent@ymin)*0.5
+
+studyAreaL <- as(largeExtent, "SpatialPolygons")
+studyAreaL <-  SpatialPolygonsDataFrame(studyAreaL, data.frame(id = 1:length(studyAreaL)))
+crs(studyAreaL) <- originalcrs
+studyAreaL <- spTransform(studyAreaL, originalcrs)  ## add original projection
+studyAreaL <- spTransform(studyAreaL, Biomass_corecrs) ## now reproject to Biomass_core standard
+
+studyAreaS <- as(smallExtent, "SpatialPolygons")
+studyAreaS <-  SpatialPolygonsDataFrame(studyAreaS, data.frame(id = 1:length(studyAreaS)))
+crs(studyAreaS) <- originalcrs
+studyAreaS <- spTransform(studyAreaS, originalcrs)  ## add original projection
+studyAreaS <- spTransform(studyAreaS, Biomass_corecrs) ## now reproject to Biomass_core standard
 
 plot(studyAreaL)
 plot(studyAreaS, add = TRUE, col = "red")
